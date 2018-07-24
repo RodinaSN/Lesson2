66e616feb0cd6b43ecb15d82f449781e8a17d793
RodinaSN <Rodina-ifz@ya.ru>
2018-07-24 16:58:51 +0300

    change soft tab

--- Quadratic.rb
+++ Quadratic.rb
@@ -12,7 +12,9 @@ d = b**2 - 4 * a * c
 if d > 0
 	puts "Дискриминант = #{d}" 
   dsqrt = Math.sqrt(d)
-	puts "x1 = #{(-b + dsqrt)/ (2 * a)} and x2= #{(-b - dsqrt)/ (2 * a)}"
+  x1 = (-b + dsqrt)/ (2 * a)
+  x2 = (-b - dsqrt)/ (2 * a)
+	puts "x1 = #{x1} and x2 = #{x2}"
 
 elsif 	d == 0 
 	puts "x = #{-b / 2 * a}"
