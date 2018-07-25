e579ff644f21f9fd024692350b9fdad8dffdda64
RodinaSN <Rodina-ifz@ya.ru>
2018-07-25 11:26:26 +0300

    tab

--- Quadratic.rb
+++ Quadratic.rb
@@ -10,17 +10,17 @@ c = gets.to_f
 d = b**2 - 4 * a * c
 
 if d > 0
-	puts "Дискриминант = #{d}" 
-  dsqrt = Math.sqrt(d)
-  x1 = (-b + dsqrt)/ (2 * a)
-  x2 = (-b - dsqrt)/ (2 * a)
-	puts "x1 = #{x1} and x2 = #{x2}"
+puts "Дискриминант = #{d}" 
+dsqrt = Math.sqrt(d)
+x1 = (-b + dsqrt)/ (2 * a)
+x2 = (-b - dsqrt)/ (2 * a)
+puts "x1 = #{x1} and x2 = #{x2}"
 
 elsif 	d == 0 
-	puts "x = #{-b / 2 * a}"
+puts "x = #{-b / 2 * a}"
 
 else d < 0 
-	puts "Дискриминант = #{d}"
-	puts "Корней нет"
+puts "Дискриминант = #{d}"
+puts "Корней нет"
 
 end	
